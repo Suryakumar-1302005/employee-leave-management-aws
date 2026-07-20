5e342db3886dd196ee168cfca5d56114a3e63abf
surya <suryakumar30012005@gmail.com>
2026-07-20 22:21:31 +0530

    Frontend updated

--- app.py
+++ app.py
@@ -1,30 +1,46 @@
 from flask import Flask, render_template, request
 from db import get_connection
+import boto3
+import json
 
 app = Flask(__name__)
+
+lambda_client = boto3.client("lambda", region_name="ap-south-1")
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/apply', methods=['POST'])
 def apply():
+
     name = request.form['name']
-    days = request.form['days']
+    days = int(request.form['days'])
 
     connection = get_connection()
     cursor = connection.cursor()
 
     cursor.execute(
-        "INSERT INTO leaves (employee_name, leave_days) VALUES (%s, %s)",
-        (name, days)
+        "INSERT INTO leaves (employee_name, leave_days, status) VALUES (%s, %s, %s)",
+        (name, days, "Submitted")
     )
 
     connection.commit()
+
     cursor.close()
     connection.close()
 
-    return "Leave Applied Successfully"
+    # Invoke Lambda asynchronously
+    lambda_client.invoke(
+        FunctionName="EmployeLeave",
+        InvocationType="Event",
+        Payload=json.dumps({}).encode("utf-8")
+    )
+
+    return render_template("success.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
